13c7953c25f82f9e9168890a868088045d1990ce
Jarne Thys <jarne.thys@student.uhasselt.be>
2022-05-24 20:07:02 +0200

    Add operations and types

--- includes/instruction.h
+++ includes/instruction.h
@@ -13,38 +13,61 @@ class InstructionNumber {
 public:
     enum class Section { TYPE=0x01, IMPORT=0x02, FUNCTION=0X03, TABLE=0x04, MEMORY=0x05, GLOBAL=0x06, EXPORT=0x07, START=0x08, ELEMENT=0x09, CODE=0x0a, DATA=0x0b, CUSTOM=0x00 };
     enum class Type { NONE=0x00, I32=0x7f, I64=0x7e, F32=0x7d, F64=0x7c, FUNC=0x60 };
-    /*enum class Operation{ NONE=0x00, LOCALGET=0x20, LOCALSET=0x21, I32ADD=0x6a, I32SUB = 0x6b, I32MUL=0x6c, I32DIV=0x6d , F32ADD=0x92 , F32SUB = 0x93, F32MUL = 0x94, END=0x0b, CALL=0x10,
-                          I32CONST=0x41, I32EQ=0x46, IF=0x04, ELSE=0x05 };*/
 
-    static uint8_t getOperation(std::string name) {
+    static uint8_t getOperation(const std::string& name) {
         if ( name == "local.get" ) return constants::LOCALGET;
         if ( name == "local.set" ) return constants::LOCALSET;
+
         if ( name == "i32.add" ) return constants::I32ADD;
         if ( name == "i32.sub" ) return constants::I32SUB;
+        if ( name == "i32.mul" ) return constants::I32MUL;
         if ( name == "i32.const" ) return constants::I32CONST;
         if ( name == "i32.eq" ) return constants::I32EQ;
+
+        if (name == "f32.add") return constants::F32ADD;
+        if (name == "f32.sub") return constants::F32SUB;
+        if (name == "f32.mul") return constants::F32MUL;
+
         if ( name == "if" ) return constants::IF;
         if ( name == "else" ) return constants::ELSE;
         if ( name == "call" ) return constants::CALL;
         if ( name == "end" ) return constants::BLOCK_END;
         // TODO: support other operations!
 
+        printf("Unsupported operation: %s\n", name.c_str());
         return 0;
     }
 
-    static Type getType(std::string name) {
+    static Type getType(const std::string& name) {
         if ( name == "i32" ) return Type::I32;
-        // TODO: support other types!
+        if ( name == "i64" ) return Type::I64;
+        if ( name == "f32" ) return Type::F32;
+        if ( name == "f64" ) return Type::F64;
 
         return Type::NONE;
     }
 
     static bool isCalculation( uint8_t op ) {
-        return op == constants::I32ADD; // TODO: allow more calculations than just i32.add!
+        const std::vector<uint8_t> calcOPs = {
+                constants::I32ADD, constants::I32SUB, constants::I32MUL,
+                constants::I64ADD, constants::I64SUB, constants::I64MUL,
+                constants::F32ADD, constants::F32SUB, constants::F32MUL,
+                constants::F64ADD, constants::F64SUB, constants::F64MUL
+        };
+        for (uint8_t calcOP : calcOPs) {
+            if ( calcOP == op ) return true;
+        }
+        return false;
     }
 
     static bool isConst( uint8_t op ) {
-        return op == constants::I32CONST; // TODO: allow more consts than just i32
+        const std::vector<uint8_t> constOPs = {
+                constants::I32CONST, constants::I64CONST, constants::F32CONST, constants::F64CONST
+        };
+        for (uint8_t constOP : constOPs) {
+            if ( constOP == op ) return true;
+        }
+        return false;
     }
 
     static bool hasParameter( uint8_t op ) {
@@ -52,12 +75,12 @@ public:
     }
 
     static bool hasNoParameter( uint8_t op ) {
-        return 
-            op == constants::IF ||
-            op == constants::ELSE ||
-            op == constants::BLOCK_END ||
-            op == constants::I32EQ ||
-            op == 0;
+        const std::vector<uint8_t> noParamOPs = {
+                constants::BLOCK_END, constants::IF, constants::ELSE, constants::CALL
+        };
+        for (uint8_t noParamOP : noParamOPs) {
+            if ( noParamOP == op ) return true;
+        }
     }
 };
 
