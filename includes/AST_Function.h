f57e3c0f4589a0b9e414ba8c5dc7be659aff1629
Jarne Thys <jarne.thys@student.uhasselt.be>
2022-05-24 20:05:53 +0200

    Save local variables

--- includes/AST_Function.h
+++ includes/AST_Function.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <string>
+#include <unordered_map>
 #include "instruction.h"
 
 enum class VariableType { is_int32, is_int64, isfloat32_t, isfloat64_t };
@@ -12,6 +13,7 @@ typedef struct AST_Function {
     std::vector<VariableType> parameters = {};
     std::vector<VariableType> results = {};
     std::vector<Instruction*>* body = nullptr;
+    std::unordered_map<std::string, std::pair<uint32_t, uint8_t>> locals = {};
 } AST_Function;
 
 
